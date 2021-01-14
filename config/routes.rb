49c19831250f60f2da92254844d702cf2a0b6dfc
3balljuggler <3balljuggling@gmail.com>
2021-01-14 12:32:22 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tricks#index'
   get 'tricks', to: 'tricks#performance'
-  resources :tricks, only: [:index]
+  resources :tricks, only: [:index, :performance]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
